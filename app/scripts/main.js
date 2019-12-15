7b66951bddf2641d6dad9f9202e1ba9fe1a98e29
Igor Bezmestin <igor-rock@list.ru>
2019-12-15 17:09:28 +0300

    Сделал добавление каждый отзыв в определенный placemark на карту

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -23,72 +23,51 @@ function init() {
         behaviors: ['drag'] // зададим поведение: drag - позволяет перемещать карту при нажатии ЛКМ
     });
 
-    /* let clusterer = new ymaps.Clusterer({
-        clustererIcons: [
-            {
-                href: 'images/sprite.png',
-                size: [107, 79],
-                offset: [-81, -79]
-            }
-        ]
-    }); */
-
     // обработчик клика на карте и получение координат
     myMap.events.add('click', event => {
         coordinates = [
             event.get('coords')[0].toPrecision(6),
             event.get('coords')[1].toPrecision(6)//.toPrecision(6)
         ];
-        //isClicked = true;
+        isClicked = true;
 
-        geoObjects.push(new ymaps.Placemark(
-            [
-                coordinates[0], // latitude
-                coordinates[1]  // longitude
-            ],
-            {
-                hintContent: '1234567'
-            },
-            {
-                iconLayout: 'default#image',
-                iconImageHref: 'images/sprite.png',
-                iconImageSize: [44, 66],
-                iconImageOffset: [-22, -66],
-                iconImageClipRect: [[10, 10], [54, 76]]
-            }));
-
-        geoObjects.forEach(obj => {
-            obj.events.add('mouseenter', event => {
-                // Ссылку на объект, вызвавший событие, можно получить из поля 'target'
-                // Метод set задает значения опций для данного менеджера (https://tech.yandex.ru/maps/archive/doc/jsapi/2.0/ref/reference/option.Manager-docpage/)
-                event.get('target').options.set(
+        /*         geoObjects.push(new ymaps.Placemark(
+                    [
+                        coordinates[0], // latitude
+                        coordinates[1]  // longitude
+                    ],
                     {
-                        iconImageClipRect: [[74, 10], [118, 76]]
-                    }
-                );
-            })
-            .add('mouseleave', event => {
-                event.get('target').options.set(
+                        hintContent: '1234567'
+                    },
                     {
+                        iconLayout: 'default#image',
+                        iconImageHref: 'images/sprite.png',
+                        iconImageSize: [44, 66],
+                        iconImageOffset: [-22, -66],
                         iconImageClipRect: [[10, 10], [54, 76]]
-                    }
-                );
-            });
-        });
-
-        let clusterer = new ymaps.Clusterer({
-            clustererIcons: [
-                {
-                    href: 'images/sprite.png',
-                    size: [107, 79],
-                    //offset: [-81, -79]
-                    offset: [0, 0]
-                }
-            ]
-        });
-        clusterer.add(geoObjects);
-        myMap.geoObjects.add(clusterer);
-
+                    }));
+        
+                geoObjects.forEach(obj => {
+                    obj.events.add('mouseenter', event => {
+                        // Ссылку на объект, вызвавший событие, можно получить из поля 'target'
+                        // Метод set задает значения опций для данного менеджера (https://tech.yandex.ru/maps/archive/doc/jsapi/2.0/ref/reference/option.Manager-docpage/)
+                        event.get('target').options.set(
+                            {
+                                iconImageClipRect: [[74, 10], [118, 76]]
+                            }
+                        );
+                    })
+                        .add('mouseleave', event => {
+                            event.get('target').options.set(
+                                {
+                                    iconImageClipRect: [[10, 10], [54, 76]]
+                                }
+                            );
+                        });
+                });
+        
+                clusterer.add(geoObjects);
+                myMap.geoObjects.add(clusterer); */
     });
 }
 
@@ -102,56 +81,49 @@ document.addEventListener('click', event => {
     if (target.tagName === 'YMAPS' && isClicked) {
         // отрисуем попап и сохраним его в переменную
         const popup = renderPopup(event);
-
-        // найдем кнопку "Добавить" отзыв в попапе
-        const popupButton = popup.querySelector('.form__button');
-
-        // и навешиваем обработчик кликов для добавления информации в основной массив
-        popupButton.addEventListener('click', event => {
-            // найдем форму для отзывов
-            const formReview = document.forms.formreview;
-
-            // найдем необходимые поля (имя, место, впечатления)
-            const reviewName = formReview.reviewName;
-            const reviewPlace = formReview.reviewPlace;
-            const reviewImpressions = formReview.reviewImpressions;
-
-            // проверим введенные данные (валидация)
-            if (validateInputs([reviewName, reviewPlace, reviewImpressions])) {
-                // проверим, есть в основном массиве текущие координаты
-                for (const item of listReviews) {
-                    if (item.coordinates[0] === coordinates[0] && item.coordinates[1] === coordinates[1]) {
-                        item.reviews.push({
-                            name: reviewName.value,
-                            place: reviewPlace.value,
-                            date: '13.12.2015', // FIXME: исправить считывание даты
-                            text: reviewImpressions.value,
-                        });
-                        console.log(listReviews);
-                        ymaps.ready(renderMarks(listReviews, myMap));
-                        return;
-                    }
-                }
-
-                addInformation({ reviewName, reviewPlace, reviewImpressions });
-
-                console.log(listReviews);
-                ymaps.ready(renderMarks(listReviews, myMap));
-            } else {
-                return;
-            }
-        });
-
-        isClicked = false;
     }
 });
 
+// -----------------------------------------------------
+// Обработчик скрытия попапа по отпусканию клавиши <ESC>
+// -----------------------------------------------------
+document.addEventListener('keyup', event => {
+    const popup = document.querySelector('.popup__container');
+
+    if (popup && event.keyCode === 27) {
+        event.preventDefault();
+        popup.innerHTML = '';
+    }
+})
+
+// ---------------------------------
+// Функция рендеринга меток на карте
+// ---------------------------------
 function renderMarks(array, map) {
+    let clusterer = new ymaps.Clusterer(
+        {
+            clusterIcons: [
+                {
+                    href: 'images/group.png',
+                    size: [107, 79],
+                    offset: [-81, -79]
+                }
+            ],
+            // cделаем макет содержимого иконки кластера, в котором цифры будут раскрашены по стилю класса .cluster__text
+            clusterIconContentLayout: ymaps.templateLayoutFactory.createClass(
+                '<div class="cluster__text">{{ properties.geoObjects.length }}</div>'
+            )
+        }
+    );
+
+    geoObjects = []; // обнулим массив меток на карте
+    map.geoObjects.removeAll(); // сотрем все метки на карте
+
     for (let i = 0; i < array.length; i++) {
-        geoObjects[i] = new ymaps.Placemark(
+        geoObjects.push(new ymaps.Placemark(
             [
-                parseFloat(array[i].coordinates[0]), // latitude
-                parseFloat(array[i].coordinates[1])  // longitude
+                array[i].coordinates[0], // latitude
+                array[i].coordinates[1]  // longitude
             ],
             {
                 hintContent: array[i].reviews.place
@@ -162,34 +134,41 @@ function renderMarks(array, map) {
                 iconImageSize: [44, 66],
                 iconImageOffset: [-22, -66],
                 iconImageClipRect: [[10, 10], [54, 76]]
-            });
+            }));
     }
-    let clusterer = new ymaps.Clusterer({
-        clustererIcons: [
-            {
-                href: 'images/group.png',
-                size: [107, 79],
-                offset: [-81, -79]
-                //offset: [138, 10]
-            }
-        ]
+
+    geoObjects.forEach(obj => {
+        obj.events.add('mouseenter', event => {
+            // Ссылку на объект, вызвавший событие, можно получить из поля 'target'
+            // Метод set задает значения опций для данного менеджера (https://tech.yandex.ru/maps/archive/doc/jsapi/2.0/ref/reference/option.Manager-docpage/)
+            event.get('target').options.set(
+                {
+                    iconImageClipRect: [[74, 10], [118, 76]]
+                }
+            );
+        })
+            .add('mouseleave', event => {
+                event.get('target').options.set(
+                    {
+                        iconImageClipRect: [[10, 10], [54, 76]]
+                    }
+                );
+            })
+            .add('click', event => {
+                //event.get('target').geometry.getCoordinates();
+                //console.log(event.get('target').geometry.getCoordinates());
+                coordinates = event.get('target').geometry.getCoordinates();
+                event.get('target').options.set(
+                    {
+                        iconImageClipRect: [[10, 10], [54, 76]]
+                    }
+                );
+                renderPopup(event.originalEvent.domEvent.originalEvent);
+            });
     });
-    console.log('geoObjects=', geoObjects);
 
     clusterer.add(geoObjects);
     map.geoObjects.add(clusterer);
-
-
-    /* for (const geoObj in geoObjects) {
-        geoObj.events.add('mouseenter', event => {
-            // Ссылку на объект, вызвавший событие,
-            // можно получить из поля 'target'.
-            //e.get('target').options.set('preset', 'islands#greenIcon');
-        })
-        .add('mouseleave', event => {
-            //e.get('target').options.unset('preset');
-        });
-    } */
 }
 
 // -----------------------------------------------
@@ -198,6 +177,22 @@ function renderMarks(array, map) {
 function addInformation({ reviewName, reviewPlace, reviewImpressions }) {
     let reviews = listReviews.reviews;
 
+    // проверим, есть ли в основном массиве текущие координаты
+    for (const item of listReviews) {
+        // если есть
+        if (item.coordinates[0] === coordinates[0] && item.coordinates[1] === coordinates[1]) {
+            // то добавить информацию только в массив с отзывами
+            item.reviews.push({
+                name: reviewName.value,
+                place: reviewPlace.value,
+                date: '13.12.2015', // FIXME: исправить считывание даты
+                text: reviewImpressions.value,
+            });
+            return;
+        }
+    }
+
+    // если не нашли placemark c этимиже координатами, то добавим всю информацию в основной массив как новый элемент
     if (!reviews) {
         reviews = [];
     }
@@ -220,25 +215,8 @@ function addInformation({ reviewName, reviewPlace, reviewImpressions }) {
 // ----------------------
 function renderPopup(event) {
     const WIDTH = 380, HEIGHT = 555; // габариты попапа
-
     let [x, y] = [event.clientX, event.clientY]; // координаты Х и У клика
-
-    // проверим, пустой ли наш основной массив с отзывами
-    const isEmptyListReviews = listReviews.length > 0 ? true : false;
-
-    let isFound = false; // признак наличия в основном массиве отзывов (по координатам)
-    // проверим, есть в основном массиве текущие координаты
-    for (const item of listReviews) {
-        if (item.coordinates[0] === coordinates[0] && item.coordinates[1] === coordinates[1]) {
-            isFound = true;
-            break;
-        }
-    }
-
-    // Сгенерируем сожержимое попапа в зависимости от listReviews и его длины
-    const popupTemplate = document.querySelector('#popup-template').textContent;
-    const render = Handlebars.compile(popupTemplate);
-    const html = render({ isFound, listReviews });
+    const html = generateTemplate();
 
     // ищем элемент, в который будем ложить попап
     const popup = document.querySelector('.popup');
@@ -273,7 +251,59 @@ function renderPopup(event) {
         popup.innerHTML = '';
     });
 
+    // найдем кнопку "Добавить" отзыв в попапе
+    const popupButton = popup.querySelector('.form__button');
+
+    // и навешиваем обработчик кликов для добавления информации в основной массив
+    popupButton.addEventListener('click', event => {
+        // найдем форму для отзывов
+        const formReview = document.forms.formreview;
+
+        // найдем необходимые поля (имя, место, впечатления)
+        const reviewName = formReview.reviewName;
+        const reviewPlace = formReview.reviewPlace;
+        const reviewImpressions = formReview.reviewImpressions;
+
+        // проверим введенные данные (валидация)
+        if (validateInputs([reviewName, reviewPlace, reviewImpressions])) {
+
+            addInformation({ reviewName, reviewPlace, reviewImpressions });
+
+            //console.log(listReviews);
+            ymaps.ready(renderMarks(listReviews, myMap));
+        } else {
+            return;
+        }
+    });
+
+    isClicked = false;
+
     return popup;
+}
+
+// ----------------------------------------------- 
+// Функция генерации шаблона попапа по координатам
+// ----------------------------------------------- 
+function generateTemplate() {
+    let isFound = false; // признак наличия в основном массиве отзывов (по координатам)
+    const currentReviews = { // объект с массивом отзывов по текущим координатам
+        count: 0,
+        items: []
+    }; 
+
+    // проверим, есть в основном массиве текущие координаты
+    for (const item of listReviews) {
+        if (item.coordinates[0] === coordinates[0] && item.coordinates[1] === coordinates[1]) {
+            isFound = true;
+            currentReviews.count = item.reviews.length;
+            currentReviews.items = [...item.reviews];
+            break;
+        }
+    }
+    // Сгенерируем сожержимое попапа в зависимости от listReviews и его длины
+    const popupTemplate = document.querySelector('#popup-template').textContent;
+    const render = Handlebars.compile(popupTemplate);
+    return render({isFound, ...currentReviews});
 }
 
 // ----------------------------------
