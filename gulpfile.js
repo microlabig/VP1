25fa1cd9b36e40d0a6bd3e2d23d9e47380e4947a
Igor Bezmestin <igor-rock@list.ru>
2019-12-31 19:28:54 +0300

    Изменил сборку: добавлен линтер

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,34 @@
 // импортируем gulp
-const {src, dest, task, series, watch, parallel} = require('gulp');
+const { src, dest, task, series, watch, parallel } = require('gulp');
 // импортируем плагины
-const   rm = require('gulp-rm'),    // удаление файлов
-        sass = require('gulp-sass'), // SASS
-        concat = require('gulp-concat'), // соединение файлов в один
-        browserSync = require('browser-sync').create(), // dev-Server
-        reload = browserSync.reload, // функция перезагрузки dev-Server
-        sassGlob = require('gulp-sass-glob'), // продвинутый импорт стилей
-        autoprefixer = require('gulp-autoprefixer'), // автопрефиксер
-        px2rem = require('gulp-smile-px2rem'), // перевод из px в rem
-        gcmq = require('gulp-group-css-media-queries'), // группировка одинаковых медиазапросов  
-        cleanCSS = require('gulp-clean-css'), // минификация css
-        sourcemaps = require('gulp-sourcemaps'), // source maps
-        babel = require('gulp-babel'), // ES 6 -> браузеросовместимый код
-        uglify = require('gulp-uglify'), // минификация js
-        notify = require("gulp-notify"), // уведомления 
-        wait = require('gulp-wait2'), // задержка
-        // svgo = require('gulp-svgo'), // оптимизация svg
-        // svgSprite = require('gulp-svg-sprite'), // объединения всех svg в один
-        gulpif = require('gulp-if'); // плагин условия
-        // pug = require('gulp-pug'); // шаблонизатор pug
-        const eslint = require('gulp-eslint');
+const rm = require('gulp-rm'),    // удаление файлов
+    sass = require('gulp-sass'), // SASS
+    concat = require('gulp-concat'), // соединение файлов в один
+    browserSync = require('browser-sync').create(), // dev-Server
+    reload = browserSync.reload, // функция перезагрузки dev-Server
+    sassGlob = require('gulp-sass-glob'), // продвинутый импорт стилей
+    autoprefixer = require('gulp-autoprefixer'), // автопрефиксер
+    px2rem = require('gulp-smile-px2rem'), // перевод из px в rem
+    gcmq = require('gulp-group-css-media-queries'), // группировка одинаковых медиазапросов  
+    cleanCSS = require('gulp-clean-css'), // минификация css
+    sourcemaps = require('gulp-sourcemaps'), // source maps
+    babel = require('gulp-babel'), // ES 6 -> браузеросовместимый код
+    uglify = require('gulp-uglify'), // минификация js
+    notify = require("gulp-notify"), // уведомления 
+    wait = require('gulp-wait2'), // задержка
+    // svgo = require('gulp-svgo'), // оптимизация svg
+    // svgSprite = require('gulp-svg-sprite'), // объединения всех svg в один
+    gulpif = require('gulp-if'), // плагин условия
+    browserify = require('gulp-browserify');
+//polyfill = require('@babel/polyfill');
+
+// pug = require('gulp-pug'); // шаблонизатор pug
+const eslint = require('gulp-eslint');
 
 const env = process.env.NODE_ENV; // env - переменная из переменных окружения node.js, определяется в package.json
 
 // импортируем настройки из gulp.config.js
-const {SRC_PATH, DIST_PATH, STYLES_LIBS, JS_LIBS} = require('./gulp.config');
+const { SRC_PATH, DIST_PATH, STYLES_LIBS, JS_LIBS } = require('./gulp.config');
 
 // укажем компилятор для SASS
 sass.compiler = require('node-sass');
@@ -44,40 +47,50 @@ task('clean', () => {
 
 // таск стилей
 task('styles', () => {
-  return src([...STYLES_LIBS,`${SRC_PATH}/styles/main.scss`])
-    .pipe(gulpif(env === 'dev', sourcemaps.init()))
+    return src([...STYLES_LIBS, `${SRC_PATH}/styles/main.scss`])
+        .pipe(gulpif(env === 'dev', sourcemaps.init()))
         .pipe(concat('main.min.scss'))
         .pipe(sassGlob())
         //.pipe(sass().on('error', sass.logError))
         .pipe(wait(1500))
-        .pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError() ))
+        .pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
         .pipe(px2rem({
             dpr: 1,             // base device pixel ratio (default: 2)
             rem: 16,            // root element (html) font-size (default: 16)
             one: false          // whether convert 1px to rem (default: false)
-        })) 
+        }))
         .pipe(gulpif(env === 'dev', autoprefixer({ cascade: false })))
         .pipe(gulpif(env === 'build', gcmq()))
         .pipe(gulpif(env === 'build', cleanCSS()))
-    .pipe(gulpif(env === 'dev', sourcemaps.write()))
-    .pipe(dest(`${DIST_PATH}`))
-    .pipe(reload({stream: true}));
+        .pipe(gulpif(env === 'dev', sourcemaps.write()))
+        .pipe(dest(`${DIST_PATH}`))
+        .pipe(reload({ stream: true }));
 });
 
 // таск скриптов
 task('scripts', () => {
-    return src([...JS_LIBS,`${SRC_PATH}/scripts/**/*.js`])
-    .pipe(gulpif(env === 'dev', sourcemaps.init()))
-    .pipe(concat('main.min.js', {newLine: ";"}))
-    .pipe(gulpif(env === 'build', babel({ presets: ['@babel/env'] })))
-    .pipe(gulpif(env === 'build', uglify()))
-    .pipe(gulpif(env === 'dev', sourcemaps.write()))
-    .pipe(dest(`${DIST_PATH}`))
-    .pipe(reload({stream: true}));
+    return src([...JS_LIBS, `${SRC_PATH}/scripts/**/*.js`])
+        .pipe(gulpif(env === 'dev', sourcemaps.init()))
+        .pipe(concat('main.min.js', { newLine: ";" }))
+        .pipe(gulpif(env === 'build', babel(
+            {
+                presets: ["@babel/preset-env"],
+                plugins: [
+                    ["@babel/plugin-transform-runtime", { "regenerator": true }]
+                ]
+            }
+        )))
+        .pipe(browserify({
+            insertGlobals: true
+        }))
+        .pipe(gulpif(env === 'build', uglify()))
+        .pipe(gulpif(env === 'dev', sourcemaps.write()))
+        .pipe(dest(`${DIST_PATH}`))
+        .pipe(reload({ stream: true }));
 });
 
 task('eslint', () => {
-    return src([...JS_LIBS,`${SRC_PATH}/scripts/**/*.js`])
+    return src([...JS_LIBS, `${SRC_PATH}/scripts/**/*.js`])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError())
@@ -146,7 +159,7 @@ task('server', () => {
 
 
 // вотчеры
-task('watch', ()=> {
+task('watch', () => {
     //watch(`./${SRC_PATH}/pages/**/*.pug`, series('pug'));
     watch(`./${SRC_PATH}/pages/**/*.html`, series('copy:html'));
     watch(`./${SRC_PATH}/styles/**/*.scss`, series('styles'));
@@ -155,24 +168,24 @@ task('watch', ()=> {
 })
 
 // таск по умолчанию (gulp)
-task('default', 
-    series('clean', 
-            parallel('copy:img', 'copy:fonts', 'copy:favicon', 'copy:html'), 
-            parallel('styles', 'scripts', /* 'icons', */ ), 
-            parallel('watch', 'server')
+task('default',
+    series('clean',
+        parallel('copy:img', 'copy:fonts', 'copy:favicon', 'copy:html'),
+        parallel('styles', 'scripts'),
+        parallel('watch', 'server')
     )
 );
 
 // таск build
-task('build', 
-    series('clean', 
-        parallel('copy:img', 'copy:fonts', 'copy:favicon', 'copy:html'), 
-        parallel('styles', 'scripts', /* 'icons', */ )
+task('build',
+    series('clean',
+        parallel('copy:img', 'copy:fonts', 'copy:favicon', 'copy:html'),
+        parallel('styles', 'scripts')
     )
 );
 
 // таск checkJS
-task('codestyle', 
+task('codestyle',
     series('eslint')
 );
 
